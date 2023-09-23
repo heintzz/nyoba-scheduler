a9f9eedc56fe1d592d6b3394771f769ef5ab7c74
heintzz <mhasnanr@gmail.com>
2023-09-23 08:53:24 +0700

    feat: add income model

---
+++ model/income.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const incomeSchema = new Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  is_monthly_income: {
+    type: Boolean,
+    default: false,
+  },
+  has_recreated: {
+    type: Boolean,
+    default: false,
+  },
+  created_at: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+module.exports = mongoose.model("Income", incomeSchema);
